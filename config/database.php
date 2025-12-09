bc1d684c62476eaa9d00787d7fa0bc641de39148
vandalier7 <jahadielduran26@gmail.com>
2025-12-09 19:37:05 +0800

    disabled local database connection requirements

--- config/database.php
+++ config/database.php
@@ -16,7 +16,9 @@ return [
     |
     */
 
+    // 'default' => env('DB_CONNECTION', 'sqlite'),
     'default' => env('DB_CONNECTION', 'sqlite'),
+
 
     /*
     |--------------------------------------------------------------------------
